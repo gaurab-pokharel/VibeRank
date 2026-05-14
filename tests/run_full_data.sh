824bc8f7087d78994ae4a7cccc9608fbc6ccee97
Farabi Shafkat Rahman <farabi.shafkat@gmail.com>
2026-05-14 17:43:59 -0400

    running on slurm

--- tests/run_full_data.sh
+++ tests/run_full_data.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH -J RCQwentayvispdat1
+#SBATCH -J RCQDSvispdat
 #SBATCH --account=simlai1
 #SBATCH --partition=a100_normal_q
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
-#SBATCH --time=0-12:00:00
+#SBATCH --time=0-5:00:00
 #SBATCH --gres=gpu:1
-#SBATCH --output=logsRC/FullData_llama_vispdat_%j.out
-#SBATCH --error=logsRC/FullData_llama_vispdat_%j.err
+#SBATCH --output=logsRC/FullData_qwen2_vispdat_%j.out
+#SBATCH --error=logsRC/FullData_qwen2_vispdat_%j.err
 cd /projects/simlai1/Viberank/VibeRank/tests
 set -euo pipefail
 mkdir -p logsPairwiseVul
--- tests/test_full_data.py
+++ tests/test_full_data.py
@@ -28,7 +28,7 @@ config_path = Path("/projects/simlai1/Viberank/VibeRank/configs/datasets/rc_full
 dataloader = HMISPairwiseDataLoader.from_yaml(config_path)
 dataloader.prepare()
 
-run_id = datetime.now().strftime("AIES_vispdat_llama7_NewFixedLogger")
+run_id = datetime.now().strftime("AIES_vispdat_QWEN_NewFixedLoggerRun1SeedFixed")
 log_path = dataloader.config.responses_dir / f"{run_id}.jsonl"
 
 logger = JSONLResponseLogger(
@@ -53,8 +53,8 @@ comp = LLMComparator(
     **comp_kwargs,
     num_samples=dataloader.config.run_settings.get("repeats_per_ordered_pair", 2),
     logger = logger,
-    rng_seed=42, 
-    llm_name = 'llama7', # deepseek8B / llama7 / qwen
+    rng_seed=43, 
+    llm_name = 'qwen', # deepseek8B / llama7 / qwen
     timeout= 120,
     max_tokens = 64,
     temperature = 0.1,
@@ -68,8 +68,8 @@ runner = RankCentralityExperimentRunner(
     dataloader=dataloader,
     logger=logger,
     comparator=comp,
-    run_id="aies_vispdat_llama_001",
-    model_name="llama7",
+    run_id="aies_vipdat_qwen_002Cor",
+    model_name="qwen",
     prompt_version="v1",
 )
 
